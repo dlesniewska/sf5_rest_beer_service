205bbe61d9143920aca39d532abbb89904cf5305
dlesniewska <32817937+dlesniewska@users.noreply.github.com>
2020-01-19 22:02:26 +0100

    Mysql local - fixing hibernate error and artemis disable. Closes #28

--- src/main/java/dl/springframework/sf5_rest_beer_service/Sf5RestBeerServiceApplication.java
+++ src/main/java/dl/springframework/sf5_rest_beer_service/Sf5RestBeerServiceApplication.java
@@ -2,8 +2,9 @@ package dl.springframework.sf5_rest_beer_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jms.artemis.ArtemisAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = ArtemisAutoConfiguration.class)
 public class Sf5RestBeerServiceApplication {
 
     public static void main(String[] args) {
