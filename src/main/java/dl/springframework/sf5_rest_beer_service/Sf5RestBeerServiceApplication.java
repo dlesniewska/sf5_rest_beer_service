901591eacbe1eb58bb5bd0996e85db295307912a
dlesniewska <32817937+dlesniewska@users.noreply.github.com>
2020-01-26 21:51:00 +0100

    Added JMS config - using real jms local. Closes #34

--- src/main/java/dl/springframework/sf5_rest_beer_service/Sf5RestBeerServiceApplication.java
+++ src/main/java/dl/springframework/sf5_rest_beer_service/Sf5RestBeerServiceApplication.java
@@ -2,9 +2,8 @@ package dl.springframework.sf5_rest_beer_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jms.artemis.ArtemisAutoConfiguration;
 
-@SpringBootApplication(exclude = ArtemisAutoConfiguration.class)
+@SpringBootApplication
 public class Sf5RestBeerServiceApplication {
 
     public static void main(String[] args) {
