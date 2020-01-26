716810b6573705bf446eb25f04a8510aec0da917
dlesniewska <32817937+dlesniewska@users.noreply.github.com>
2020-01-26 21:50:06 +0100

    Added BeerService. Closes #34

---
+++ src/main/java/dl/springframework/sf5_rest_beer_service/web/services/BrewingService.java
@@ -0,0 +1,44 @@
+package dl.springframework.sf5_rest_beer_service.web.services;
+
+import dl.springframework.sf5_rest_beer_service.config.JmsConfig;
+import dl.springframework.sf5_rest_beer_service.domain.Beer;
+import dl.springframework.sf5_rest_beer_service.events.BrewBeerEvent;
+import dl.springframework.sf5_rest_beer_service.repositories.BeerRepository;
+import dl.springframework.sf5_rest_beer_service.web.mappers.BeerMapper;
+import dl.springframework.sf5_rest_beer_service.web.services.inventory.BeerInventoryService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Slf4j
+@Service
+public class BrewingService {
+
+    private final BeerRepository beerRepository;
+    private final BeerInventoryService beerInventoryService;
+    private final JmsTemplate jmsTemplate;
+    private final BeerMapper beerMapper;
+
+    @Scheduled(fixedRate = 5000) //every 5 seconds
+    public void checkForLowInventory(){
+        List<Beer> beers = beerRepository.findAll();
+
+        beers.forEach(beer -> {
+            Integer invQOH = beerInventoryService.getOnhandInventory(beer.getId());
+
+            log.debug("Min Onhand is: " + beer.getMinOnHand());
+            log.debug("Inventory is: "  + invQOH);
+
+            if(beer.getMinOnHand() >= invQOH){
+                jmsTemplate.convertAndSend(JmsConfig.BREWING_REQUEST_QUEUE, new BrewBeerEvent(beerMapper.beerToBeerDto(beer)));
+            }
+        });
+
+    }
+
+}
